a889267dd8986b5a4610f0c62850c95326d2efe2
Medokser <alaziz.200223@gmail.com>
2023-02-12 15:31:39 +0600

    test

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Enderstyles/Best-team
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	golang.org/x/crypto v0.6.0
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	//"golang.org/x/crypto/bcrypt"
+)
+
+
+type User struct {
+	Id 			int
+	Fullname 	string
+	Email 		string
+    Username 	string
+    Password 	string
+}
+const (  
+    username = "root"
+    password = ""
+    hostname = "127.0.0.1:3306"
+    dbname   = "golang_project"
+)
+func dsn(dbName string) string {  
+    return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
+}
+func Connect(){
+	db, err := sql.Open("mysql", dsn(dbname))
+    if err != nil {
+        panic(err.Error())
+    }
+    res, err := db.Query("SELECT * FROM users")    
+
+    defer db.Close()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for res.Next(){
+        var user User
+        err := res.Scan(&user.Id, &user.Email, &user.Fullname, &user.Username, &user.Password)
+
+        if err != nil{
+            log.Fatal(err)
+        }
+        fmt.Print("%\n",user)
+    }
+}
+
+func main() {
+    
+    http.HandleFunc("/", a.Index)
+	
+	http.HandleFunc("/register", a.Register)
+
+	fmt.Println("Server jalan di: http://localhost:3000")
+	http.ListenAndServe(":3000", nil)
+    
+
+}
