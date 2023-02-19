d5eb46b7413c7be54e22a4957ae9a1144321d7a9
medeubekovv17 <200103154@stu.sdu.edu.kz>
2023-02-19 22:14:36 +0600

    fixed login and registration errors

--- main.go
+++ main.go
@@ -3,131 +3,128 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"net/http"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"golang.org/x/crypto/bcrypt"
+	"net/http"
 )
-
 
 type User struct {
-	Id 			int
-	Fullname 	string
-	Email 		string
-    Username 	string
-    Password 	string
+	ID       int
+	Fullname string
+	Email    string
+	Username string
+	Password string
 }
-const (  
-    username = "root"
-    password = ""
-    hostname = "127.0.0.1:3306"
-    dbname   = "golang_project"
-)
-func dsn(dbName string) string {  
-    return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
-}
-func Connect(){
-	db, err := sql.Open("mysql", dsn(dbname))
-    if err != nil {
-        panic(err.Error())
-    }
-    res, err := db.Query("SELECT * FROM users")    
 
-    defer db.Close()
+func Connect() error {
+	var err error
+	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang_project")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	res, err := db.Query("SELECT * FROM users")
 
-    for res.Next(){
-        var user User
-        err := res.Scan(&user.Username, &user.Password)
+	if err != nil {
+		return err
+	}
 
-        if err != nil{
-            log.Fatal(err)
-        }
-        fmt.Print("%\n",user)
-    }
+	for res.Next() {
+		var user User
+		err := res.Scan(&user.Username, &user.Password)
+
+		if err != nil {
+			return err
+		}
+		fmt.Println("%\n", user)
+	}
+	return nil
 }
 
 var db *sql.DB
 var err error
 
-func signupPage(res http.ResponseWriter, req *http.Request) {
-	if req.Method != "POST" {
-		http.ServeFile(res, req, "views/register.html")
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "views/register.html")
 		return
 	}
 
-	username := req.FormValue("username")
-	password := req.FormValue("password")
-
-	var user string
-
-	err := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
-
-	switch {
-	case err == sql.ErrNoRows:
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-		if err != nil {
-			http.Error(res, "Server error, unable to create your account.", 500)
-			return
-		}
-
-		_, err = db.Exec("INSERT INTO users(username, password) VALUES(?, ?)", username, hashedPassword)
-		if err != nil {
-			http.Error(res, "Server error, unable to create your account.", 500)
-			return
-		}
-
-		res.Write([]byte("User created!"))
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
-	case err != nil:
-		http.Error(res, "Server error, unable to create your account.", 500)
-		return
-	default:
-		http.Redirect(res, req, "/", http.StatusMovedPermanently)
 	}
+
+	full_name := r.FormValue("full_name")
+	email := r.FormValue("email")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	_, err = db.Exec("INSERT INTO users (full_name, email, username, password) VALUES (?, ?, ?, ?)", full_name, email, username, hashedPassword)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Redirect to login page
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
-func loginPage(res http.ResponseWriter, req *http.Request) {
-	if req.Method != "POST" {
-		http.ServeFile(res, req, "views/login.html")
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "views/login.html")
 		return
 	}
 
-	username := req.FormValue("username")
-	password := req.FormValue("password")
-
-	var databaseUsername string
-	var databasePassword string
-
-	err := db.QueryRow("SELECT username, password FROM users WHERE username=?", username).Scan(&databaseUsername, &databasePassword)
-
+	err := r.ParseForm()
 	if err != nil {
-		http.Redirect(res, req, "/login", http.StatusMovedPermanently)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
 
-	err = bcrypt.CompareHashAndPassword([]byte(databasePassword), []byte(password))
+	// Retrieve user from database
+	var user User
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
-		http.Redirect(res, req, "/login", http.StatusMovedPermanently)
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+		return
+	}
+	row := db.QueryRow("SELECT id, username, password FROM users WHERE username = ?", username)
+	err = row.Scan(&user.ID, &user.Username, &user.Password)
+	if err != nil {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
 		return
 	}
 
-	res.Write([]byte("Hello" + databaseUsername))
+	// Check password
+	if user.Password != password {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
 
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func homePage(res http.ResponseWriter, req *http.Request) {
+func home(res http.ResponseWriter, req *http.Request) {
 	http.ServeFile(res, req, "views/index.html")
 }
 
 func main() {
 	Connect()
 
-	http.HandleFunc("/register", signupPage)
-	http.HandleFunc("/login", loginPage)
-	http.HandleFunc("/", homePage)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+
+	// Register routes
+	r.HandleFunc("/register", register)
+	r.HandleFunc("/login", login)
+	r.HandleFunc("/", home)
+
+	fmt.Print("Server path: http://localhost:3000")
+	http.ListenAndServe(":3000", r)
 }
